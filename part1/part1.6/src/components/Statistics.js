1bf368ab5ccbf049bdc65300e070c5ecd8fdd35e
Miron Lončarić <miron.loncaric@gmail.com>
2023-07-28 09:43:13 +0200

    Finished part 1.9

--- part1/part1.6/src/components/Statistics.js
+++ part1/part1.6/src/components/Statistics.js
@@ -1,20 +1,22 @@
 const Statistics = ({ good, neutral, bad, total, positive, average }) => {
-	return (
-		<div>
-			<h1>statistics</h1>
-			good: {good}
-			<br />
-			neutral: {neutral}
-			<br />
-			bad: {bad}
-			<br />
-			total: {total}
-			<br />
-			average: {average}
-			<br />
-			positive: {positive}
-		</div>
-	);
+	if (total !== 0)
+		return (
+			<div>
+				<h1>statistics</h1>
+				good: {good}
+				<br />
+				neutral: {neutral}
+				<br />
+				bad: {bad}
+				<br />
+				total: {total}
+				<br />
+				average: {average}
+				<br />
+				positive: {positive}
+			</div>
+		);
+	else return <p>No feedback has been given.</p>;
 };
 
 export default Statistics;
